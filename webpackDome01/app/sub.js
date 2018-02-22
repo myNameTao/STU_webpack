c3cc2d4ad24f965d149793233f5bfe16133f33be
myNameTao <1130193159@qq.com>
2018-02-22 22:41:50 +0800

    1、初探webpack

---
+++ webpackDome01/app/sub.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2018-02-09.
+ */
+export default function(){
+    let element = document.createElement('h2');
+    element.innerHTML = 'Hello h2 world';
+    return element;
+}
+// function generateText(){
+//     let element = document.createElement('h2');
+//     element.innerHTML = 'Hello h2 world';
+//     return element;
+// }
+// module.exports = generateText;
