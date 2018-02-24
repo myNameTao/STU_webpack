593dfa5f30200612b6bb4af787893952e124e67c
lt <lt@192.168.13.109>
2018-02-24 18:01:12 +0800

    webpack_reactDome

---
+++ webpack_React01/app/components/main.jsx
@@ -0,0 +1,20 @@
+'use strict'
+import React from 'react';
+
+class Main extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+    render(){
+        return (
+            <div className="container">
+                <section className="jumbotron">
+                    <h3 className="jumbotron-heading">Search Github Users</h3>
+                </section>
+            </div>
+        )
+    }
+}
---
+++ webpack_React01/app/utils/Config.js
@@ -0,0 +1,32 @@
+const OS = require('os');//系统操作函数
+module.exports = {
+    port: 4000,
+    IPv4: 'localhost',
+    // agentSev: 'http://192.168.2.12:8501',
+    // agentSevUploadPic: 'http://192.168.2.12:8800',//图片上传地址
+    getAgent: function() {
+        return {
+            // '/operator/*': {
+            //     target: this.agentSev
+            // },
+            // '/upload/*': {
+            //     target: this.agentSevUploadPic
+            // }
+        }
+    },
+    getIP: function() {
+        try {
+            const network = OS.networkInterfaces();//获得网络接口列表。
+            this.IPv4 = network[Object.keys(network)[0]][1].address
+        } catch (e) {
+            this.IPv4 = '127.0.0.1';
+        } finally {
+            const httpStr = 'http://' + this.IPv4 + ':' + this.port;
+            const opn = require('opn');//需要使用opn插件
+            setTimeout(function() {
+                opn(httpStr);
+            }, 20000);
+            return this.IPv4;
+        }
+    }
+};
