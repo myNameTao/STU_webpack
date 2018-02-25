eeff5c254b3093c8ef4b163038e9214f7248f3fe
myNameTao <1130193159@qq.com>
2018-02-25 17:17:47 +0800

    组件

--- webpack_React01/app/components/main.jsx
+++ webpack_React01/app/components/main.jsx
@@ -1,19 +1,25 @@
 'use strict'
 import React from 'react';
+import Search from './search';
+import Plist from './plist';
 
-class Main extends React.Component{
+export default class Main extends React.Component{
     constructor(props){
         super(props);
-        this.state = {
-
-        }
+        this.state = {keyword: ""};
+        this.refreshKeyword = this.refreshKeyword.bind(this);
+    }
+    refreshKeyword(name) {
+        this.setState({keyword: name});
     }
     render(){
         return (
             <div className="container">
                 <section className="jumbotron">
                     <h3 className="jumbotron-heading">Search Github Users</h3>
+                    <Search sendAction={this.refreshKeyword}/>
                 </section>
+                <Plist keyword={this.state.keyword}/>
             </div>
         )
     }
