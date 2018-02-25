6d6a5600a75564ab6528f2dd99d36d7cb9746034
myNameTao <1130193159@qq.com>
2018-02-25 17:17:03 +0800

    index

--- webpack_React01/app/index.jsx
+++ webpack_React01/app/index.jsx
@@ -1,26 +1,8 @@
-/**
- *程序入口
- */
+'use strict'
 import React from 'react';
 import ReactDOM,{render} from 'react-dom';
 import Main from './components/main';
 
-class App extends React.Component{
-    constructor() {
-        super();
-    }
-    render() {
-        //JSX here!
-        return (
-            <div className="container">
-                <section className="jumbotron">
-                    <h3 className="jumbotron-heading">Search Github Users</h3>
-                </section>
-            </div>
-        )
-    }
-};
-
-const app = document.createElement('div');
-document.body.appendChild(app);
-ReactDOM.render(<App />, app);
+render((
+    <Main></Main>
+) ,document.querySelector('#main'));
