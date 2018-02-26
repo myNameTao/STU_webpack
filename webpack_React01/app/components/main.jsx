b36f13177ec02eb352781361fcd87ff9bccf2cc0
lt <lt@192.168.13.109>
2018-02-26 12:43:08 +0800

    webpack_React01 1、移除findDOMNode过时的写法； 2、修改成受控组件 3、修改成非受控组件

--- webpack_React01/app/components/main.jsx
+++ webpack_React01/app/components/main.jsx
@@ -7,9 +7,10 @@ export default class Main extends React.Component{
     constructor(props){
         super(props);
         this.state = {keyword: ""};
-        this.refreshKeyword = this.refreshKeyword.bind(this);
+        //this.refreshKeyword = this.refreshKeyword.bind(this);//在元素标签绑定this了，初始化的时候就可以不需要绑定this了
     }
     refreshKeyword(name) {
+        alert('父页面：'+name);
         this.setState({keyword: name});
     }
     render(){
@@ -17,7 +18,7 @@ export default class Main extends React.Component{
             <div className="container">
                 <section className="jumbotron">
                     <h3 className="jumbotron-heading">Search Github Users</h3>
-                    <Search sendAction={this.refreshKeyword}/>
+                    <Search sendAction={this.refreshKeyword.bind(this)}/>
                 </section>
                 <Plist keyword={this.state.keyword}/>
             </div>
--- webpack_React01/app/components/search.jsx
+++ webpack_React01/app/components/search.jsx
@@ -6,21 +6,39 @@ import ReactDOM from 'react-dom';
 export default class Search extends React.Component{
     constructor(props){
         super(props);
-        this.handleSearch = this.handleSearch.bind(this);
+        this.state = {
+            value:''
+        };
+        this.handleSearch = this.handleSearch.bind(this);//没有在元素标签绑定this，初始化的时候就要绑定
     }
 
     handleSearch(){
-        let name  = ReactDOM.findDOMNode(this.refs.name).value;
+        // let name  = ReactDOM.findDOMNode(this.refs.name).value; //过时的写法
+        let name  = this.inputSearch.value;
+        // let name  = this.state.value;//受控组件
         if(name === ''){
             return;
         }
         this.props.sendAction(name);
     }
-
+    //受控组件
+    inputChange(even){
+        this.setState({
+            value: even.target.value
+        })
+    }
     render(){
         return(
             <div>
-                <input type="text" ref="name" placeholder="enter the name you wanna search"/>
+                {
+                    // value={this.state.value} onChange={this.inputChange.bind(this)} 受控组件
+                    // ref="name"使用findDOMNode不推荐
+                    //ref={input => this.inputSearch = input} 使用非受控组件 defaulrValue指定默认值
+                    //单选和多选 defaultChecked
+
+                }
+                <input type="text"  ref={input => this.inputSearch = input} placeholder="enter the name you wanna search"/>
+                <input type="text"  ref={input => this.inputTest = input} placeholder="test"/>
                 <button onClick={this.handleSearch}>Search</button>
             </div>
         )
