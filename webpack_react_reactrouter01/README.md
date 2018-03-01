4284bcdc10519fe66b3c4291c0ea7489d455e2fb
myNameTao <1130193159@qq.com>
2018-03-01 22:14:52 +0800

    react-router IndexLink 与 Link OnlyActiveOnIndex={true} 能达到同样的想过

--- webpack_react_reactrouter01/README.md
+++ webpack_react_reactrouter01/README.md
@@ -1,5 +1,9 @@
-更具官网例子学习react-router
-    1、知识点有嵌套路由\n
-    2、Link标签选中后的样式：activeStyle activeClassName\n
-    3、传参数 :userName this.props.params.userName取出 (/:userName)这样的方式是可选的参数\n
+更具官网例子、阮一峰博客学习react-router
+    1、知识点有嵌套路由
+    2、Link标签选中后的样式：activeStyle activeClassName
+    3、传参数 :userName this.props.params.userName取出 (/:userName)这样的方式是可选的参数
     4、{this.props.children || <Defalut/>} 没有访问子路由时访问Default路由，同样也可以使用IndexRoute 这样{this.props.children}
+    5、IndexLink 在path='/'，链接直接根路由/，就要使用IndexLink 不要使用Link
+        对于根路由来说，activeStyle和activeClassName会失效，或者说总是生效，因为/会匹配任何子路由。而IndexLink组件会使用路径的精确匹配。
+        另一种方法是使用Link组件的onlyActiveOnIndex属性，也能达到同样效果。
+        实际上，IndexLink就是对Link组件的onlyActiveOnIndex属性的包装。
--- webpack_react_reactrouter01/src/components/home.jsx
+++ webpack_react_reactrouter01/src/components/home.jsx
@@ -1,7 +1,7 @@
 'use strict';
 
 import React from 'react';
-import { Link } from 'react-router'
+import { Link, IndexLink } from 'react-router'
 import NavLink from './NavLink'
 import Defalut from './Defalut'
 
@@ -18,7 +18,13 @@ export default class Home extends React.Component{
                         //Link 标签上使用activeStyle 就是选中之后的样式
                         //官网例子中05 <Link {...this.props} activeClassName='active'/> 可以建一个组件，这样就可以统一列表了，不用一个个添加
                     }
-                    <li><Link to="/HomeHeader" activeStyle={{color:'red'}}>HomeHeader</Link></li>
+                    {/*链接直接根路由/这样的写法，即使没有访问此路由，它也会默认是active*/}
+                    {/*<li><Link to="/" activeStyle={{color:'red'}}>Defalut</Link></li>*/}
+                    {/*第一种使用IndexLink*/}
+                    {/*<li><IndexLink to="/" activeStyle={{color:'red'}}>Defalut</IndexLink></li>*/}
+                    {/*第二种是使用Link onlyActiveOnIndex={true}*/}
+                    <li><Link to="/" activeStyle={{color:'red'}} onlyActiveOnIndex={true}>Defalut</Link></li>
+                    <li><Link to="/HomeHeader" activeStyle={{color:'red'}} >HomeHeader</Link></li>
                     <li><NavLink to="/HomeBody" >HomeBody</NavLink></li>
                     <li><Link to="/Footer" activeClassName="lk-HomeBody">Footer</Link></li>
                 </ul>
